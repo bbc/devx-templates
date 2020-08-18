579707930cfa5615e13dc6c44aa654ed6bd6ba7c
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-08-18 16:10:47 +0100

    NO-TICKET: Create lambda

--- src/infrastructure/lambda-stack.ts
+++ src/infrastructure/lambda-stack.ts
@@ -1,17 +1,19 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
-import * as path from "path";
+import * as s3 from "@aws-cdk/aws-s3";
 
 export default class LambdaStack extends cdk.Stack {
   public constructor(scope: cdk.Construct, props: cdk.StackProps) {
     super(scope, "LambdaStackId", props);
 
+    const codeBucket = new s3.Bucket(this, "CodeBucket", {
+      bucketName: "code-bucket",
+    });
+
     new lambda.Function(this, "LambdaFunction", {
       handler: "handler",
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset(
-        path.join(__dirname, "..", "..", "dist", "lambda")
-      ),
+      code: lambda.Code.fromBucket(codeBucket, "lambda.js"),
     });
   }
 }
